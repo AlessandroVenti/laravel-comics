2d38e96361acae2cd81c78394be83ee5e1fa6109
Alessandro Venti <alessandro.venti@icloud.com>
2021-05-17 15:46:47 +0200

    header done

--- laravel-comics/resources/views/components/header.blade.php
+++ laravel-comics/resources/views/components/header.blade.php
@@ -1,3 +1,31 @@
 <header>
-     <h1>Io sono un header</h1>
+     <ul>
+          <li>DC POWER VISA</li>
+          <li>ADDITIONAL DC SITES</li>
+     </ul>
+
+     <div>
+          <img src="{{ asset('/storage/assets/dc-logo.png') }}" alt=''>
+
+          <ul>
+               <a href=""><li>CHARACTERS</li></a>
+               <a href=""><li>COMICS</li></a>
+               <a href=""><li>MOVIES</li></a>
+               <a href=""><li>TV</li></a>
+               <a href=""><li>GAMES</li></a>
+               <a href=""><li>COLLECTIBLES</li></a>
+               <a href=""><li>VIDEOS</li></a>
+               <a href=""><li>FANS</li></a>
+               <a href=""><li>NEWS</li></a>
+               <a href="">
+                    <li>
+                         <span>SHOP</span> 
+                         <i class="fas fa-chevron-down"></i>
+                    </li>
+               </a>
+          </ul>
+
+          <input type="text">
+
+     </div>
 </header>
